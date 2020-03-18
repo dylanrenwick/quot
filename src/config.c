b53fec6bbb0b133731775030b99cf7764979cb24
Dylan Renwick <skidsirongenesisdev@gmail.com>
2020-03-18 19:16:10 -0400

    Improved naming of config struct

--- src/config.c
+++ src/config.c
@@ -2,7 +2,7 @@
 #include "config.h"
 
 static int new_entry_handler(void* user, const char* section, const char* name, const char* value) {
-	struct qu_config* config = (struct qu_config*)user;
+	struct qu_config_config* config = (struct qu_config_config*)user;
 
 	#define MATCH(s, n) strcmp(section, s) == 0 && strcmp(name, n) == 0
 
@@ -12,5 +12,5 @@ static int new_entry_handler(void* user, const char* section, const char* name, 
 }
 
 int qu_config_init(char* fileName) {
-	return ini_parse(fileName, new_entry_handler, &config);
+	return ini_parse(fileName, new_entry_handler, &qu_config_values);
 }
--- src/config.h
+++ src/config.h
@@ -1,7 +1,7 @@
-struct qu_config {
+struct qu_config_config {
 	char* network_port;	
 };
 
-struct qu_config config;
+struct qu_config_config qu_config_values;
 
 int qu_config_init(char* fileName);
