f0bcd8acc47b850320efb37375975f657fb6076e
Dylan Renwick <skidsirongenesisdev@gmail.com>
2020-03-18 22:27:52 -0400

    Added parsing logic for network_port config

--- src/config.c
+++ src/config.c
@@ -1,12 +1,23 @@
+#include <stdlib.h>
+
 #include "../lib/inih/ini.h"
 #include "config.h"
+#include "log.h"
+
+static const char* const MOD = "config";
 
 static int new_entry_handler(void* user, const char* section, const char* name, const char* value) {
 	struct qu_config_config* config = (struct qu_config_config*)user;
 
 	#define MATCH(s, n) strcmp(section, s) == 0 && strcmp(name, n) == 0
-
-
+	if (MATCH("network", "port")) {
+		int port = atoi(value);
+		if (port < 1024 || port > 65535) {
+			qu_log_fatal(MOD, "Invalid network_port value in config!");
+			return 0;
+		}
+		config->network_port = (uint16_t)port;
+	}
 
 	return 1;
 }
