0e768dbaeb7ad1bd5d15ef247bc6920436433646
Dylan Renwick <skidsirongenesisdev@gmail.com>
2020-03-20 16:06:03 -0400

    Added log entries to config

--- src/config.c
+++ src/config.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -19,6 +20,15 @@ static int new_entry_handler(void* user, const char* section, const char* name, 
 			return 0;
 		}
 		config->network_port = (uint16_t)port;
+	} else if (MATCH("log", "console_lvl")) {
+		config->log_console_lvl = atoi(value);
+	} else if (MATCH("log", "file_lvl")) {
+		config->log_file_lvl = atoi(value);
+	} else if (MATCH("log", "maxfilesize")) {
+		config->log_maxfilesize = atoi(value);
+	} else if (MATCH("log", "log_path")) {
+		// TODO: Validate path
+		config->log_path = qu_util_strdup(value);
 	}
 
 	return 1;
--- src/config.h
+++ src/config.h
@@ -1,7 +1,12 @@
 #include <stdint.h>
 
 struct qu_config_config {
-	uint16_t network_port;	
+	uint16_t network_port;
+
+	int log_console_lvl;
+	int log_file_lvl;
+	uint32_t log_maxfilesize;
+	const char* log_path;
 };
 
 struct qu_config_config qu_config_values;
