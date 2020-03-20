3ffb9d23ca67e8ef227e89d9f885d9a971b5fd0d
landyacht <alexander.eubanks@protonmail.com>
2020-03-20 14:14:03 -0400

    Fixed wrong type passed to localtime

--- src/log.c
+++ src/log.c
@@ -48,7 +48,7 @@ static void qu_log_common(enum qu_log_level lvl, char* lvlstr, char* module, cha
 	char timestamp[22];
 
 	time_t curtime = time(NULL);
-	struct tm* curtime_loc = localtime(curtime);
+	struct tm* curtime_loc = localtime(&curtime);
 	sprintf(timestamp, "[%d-%02d-%02d %02d:%02d:%02d]",
 		curtime_loc->tm_year + 1900,
 		curtime_loc->tm_mon + 1,
@@ -102,7 +102,7 @@ static void qu_log_rotate() {
 	// Generate a temporal name for the archived log
 	char archive_name[24];
 	time_t curtime = time(NULL);
-	struct tm* curtime_loc = localtime(curtime);
+	struct tm* curtime_loc = localtime(&curtime);
 	sprintf(archive_name, "%d-%02d-%02d_%02d.%02d,%02d",
 		curtime_loc->tm_year + 1900,
 		curtime_loc->tm_mon + 1,
