5a9fd341a433ab434b39eb85f233aef959fcee1e
Dylan Renwick <skidsirongenesisdev@gmail.com>
2020-03-22 18:23:18 -0400

    Added --no-ascii arg

--- src/main.c
+++ src/main.c
@@ -1,13 +1,35 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
 
 #include "config.h"
 #include "netwk.h"
 
 #define ART_BUFFER_SIZE 80
-void print_ascii_art();
 
-int main(int argc, char* argv[]) {
-	print_ascii_art();
+void print_ascii_art(bool ignore_file) {
+	FILE* art_file;
+
+	if (ignore_file || !(art_file = fopen("resources/quot_ascii_art.txt", "r"))) {
+		puts("Starting Quot, a fast and flexible chat system licensed under the Affero GPL.");
+		return;
+	}
+
+	char buffer[ART_BUFFER_SIZE];
+	while (fgets(buffer, ART_BUFFER_SIZE, art_file) != NULL) {
+		fputs(buffer, stdout);
+	}
+
+	fclose(art_file);
+}
+
+int main(int argc, char** argv) {
+	bool ignore_file = false;
+	if (argc > 0 && strcmp(argv[0], "--no-ascii") == 0) {
+		ignore_file = true;
+	}
+
+	print_ascii_art(ignore_file);
 
 	// Initialize modules
 	int config_status = qu_config_init("./quot.cfg");
@@ -23,18 +45,3 @@ int main(int argc, char* argv[]) {
 	return 0;
 }
 
-void print_ascii_art() {
-	FILE* art_file;
-
-	if (! (art_file = fopen("resources/quot_ascii_art.txt", "r"))) {
-		puts("Starting Quot, a fast and flexible chat system licensed under the Affero GPL.");
-		return;
-	}
-
-	char buffer[ART_BUFFER_SIZE];
-	while (fgets(buffer, ART_BUFFER_SIZE, art_file) != NULL) {
-		fputs(buffer, stdout);
-	}
-
-	fclose(art_file);
-}
